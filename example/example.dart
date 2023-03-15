6eaef441e5cc66f972fc102ef4051411b88fbea1
Ahmet Veysel <avs@Ahmet-MacBook-Pro.local>
2023-03-15 16:53:31 +0300

    Release

--- example/example.dart
+++ example/example.dart
@@ -1,7 +1,8 @@
 import 'package:replace_html_characters/src/replace_html_characters_base.dart';
 
 void main() {
-  String data = '&#x22;Initial studies indicate that these coffins are completely closed and haven&#x27;t been opened since they were buried,&#x22; Egypt&#x27;s antiquities ministry said in a statement on Saturday.';
+  String data =
+      '&#x22;Initial studies indicate that these coffins are completely closed and haven&#x27;t been opened since they were buried,&#x22; Egypt&#x27;s antiquities ministry said in a statement on Saturday.';
 
   String modifiedData = data.replaceHTMLCharacters;
   // Output: "Initial studies indicate that these coffins are completely closed and haven't been opened since they were buried," Egypt's antiquities ministry said in a statement on Saturday.
